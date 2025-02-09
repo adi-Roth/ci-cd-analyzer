19f56f8f49181e527844128a227765bbbdd3e776
Adi Roth <adi.roth.work@gmail.com>
2025-02-09 10:00:48 +0200

    fix unitests errors

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,12 +1,12 @@
 """conftest file to override functions in my app"""
-from unittest.mock import patch
+from unittest.mock import AsyncMock, patch
 import pytest
 from app.tests.mock_data import mock_authenticate_with_ldap
 
-@pytest.fixture
+@pytest.fixture(scope="function", autouse=True)
 def override_ldap_auth():
-    """Override for app.dependencies.get_ldap_connection in a mock function"""
-    with patch("app.dependencies.get_ldap_connection", new=mock_authenticate_with_ldap):
+    """Mock `get_ldap_connection` to use our fake LDAP authentication function."""
+    with patch("app.dependencies.get_ldap_connection", new=AsyncMock(side_effect=mock_authenticate_with_ldap)):
         yield
 
 @pytest.fixture(scope="session", autouse=True)
