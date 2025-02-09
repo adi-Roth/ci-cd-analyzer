69d2df6305798302dda752341e1508c8ad1e69ca
Adi Roth <adi.roth.work@gmail.com>
2025-02-09 13:03:43 +0200

    fix unitests

--- backend/app/tests/test_routes.py
+++ backend/app/tests/test_routes.py
@@ -2,18 +2,13 @@
 from unittest.mock import patch
 import pytest
 from fastapi.testclient import TestClient
-from app.tests.conftest import override_ldap_auth
 
 with patch("app.database.init_db", return_value=None):
     from app.main import app
 
 client = TestClient(app)
 
-@pytest.fixture(autouse=True)
-def force_override_ldap_auth(override_ldap_auth):
-    """Manually apply override_ldap_auth fixture."""
-    _ = override_ldap_auth  # ✅ Ensure it's applied
-
+@pytest.mark.usefixtures("override_ldap_auth")
 def test_ldap_route_success():
     """Testing login to ldap server, result need to be 200"""
     user_data = {"username": "e029863", "password": "e029863"}
@@ -23,6 +18,7 @@ def test_ldap_route_success():
     assert response.cookies.get("access_token") is not None
     assert response.json() == {"message": "Login successful"}
 
+@pytest.mark.usefixtures("override_ldap_auth")
 def test_ldap_route_failure():
     """Testing login to ldap server, result need to be 401"""
     user_data = {"username": "e012345", "password": "somePass"}
